09e06cce53404fc87c3b07c9c4fb6732da747a00
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-02 07:27:57 -0300

    Create Program.cs

---
+++ C#/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+namespace Teste{
+    internal class Program{
+        static void Main(String[] args){
+            int n = int.Parse(Console.ReadLine());
+            int[] vet = new int[n];
+            vet = Console.ReadLine().Split(' ');
+        } 
+    }
+}
