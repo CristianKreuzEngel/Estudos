c83f286372d3abba19631b76ee154c42bb99ed1d
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-02 07:45:35 -0300

    Create 1001.php

---
+++ PHP/Questões uri/1001.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+$valor= trim(fgets(STDIN));
+$valor1= trim(fgets(STDIN));
+
+$calculo = $valor + $valor1;
+
+echo "X = " . $calculo . "\n";
+?>
