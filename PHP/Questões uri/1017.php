1cd4753c7773d8cda8c3aa9d1db62fd351d8de9a
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-02 07:45:14 -0300

    Create 1017.php

---
+++ PHP/Questões uri/1017.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+$a= trim(fgets(STDIN));
+$b=trim(fgets(STDIN));
+
+$calculo=(($a*$b)/12);
+$formato=number_format($calculo,3,'.','');
+
+echo  $formato."\n";
+
+
+?>
