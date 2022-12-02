9a2b0d590c8bcc65a0a0810ecc9ef724b1c5691f
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-02 07:45:17 -0300

    Create 1016.php

---
+++ PHP/Questões uri/1016.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+$a= trim(fgets(STDIN));
+$calculo=($a*2);
+
+echo  $calculo . " minutos". "\n";
+
+?>
