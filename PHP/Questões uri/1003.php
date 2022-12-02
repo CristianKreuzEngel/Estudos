7f3c36d0afa0c2d1a1b8ba5c7cee5a41bc8fd4af
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-02 07:45:40 -0300

    Create 1003.php

---
+++ PHP/Questões uri/1003.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+$valor= trim(fgets(STDIN));
+$valor1= trim(fgets(STDIN));
+
+$calculo = $valor + $valor1;
+
+echo "SOMA = " . $calculo . "\n";
+
+
+?>
