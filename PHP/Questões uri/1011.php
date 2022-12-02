566ec3ee7f1e41060d98e3bc892d90d0280081b0
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-02 07:45:25 -0300

    Create 1011.php

---
+++ PHP/Questões uri/1011.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+$raio=trim(fgets(STDIN));
+
+$calculo=((4/3.0)*3.14159*($raio*$raio*$raio));
+
+printf("VOLUME = %.3f\n",$calculo);
+
+
+?>
