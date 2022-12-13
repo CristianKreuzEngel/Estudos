bd5dd68857d0b2e7f4548490903d12cf1efd02f1
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-13 07:38:56 -0300

    Update main.dart

--- Flutter_Projetos/primeiro_projeto/lib/main.dart
+++ Flutter_Projetos/primeiro_projeto/lib/main.dart
@@ -1,10 +1,19 @@
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
       title: "Primeiro Projeto",
       home: Container(
-        color: Colors.white,
-        child: Text("Exemplo container"),
-      )));
+          color: Colors.white,
+          child: Column(
+            children: [
+              Text("texto 01"),
+              Text("texto 02"),
+              Text("texto 03")
+          ]
+        )
+      )
+    )
+  );
 }
