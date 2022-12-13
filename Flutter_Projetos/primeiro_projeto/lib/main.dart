ade293c648b066e781c5005069c835720a2ebcbb
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2022-12-13 07:31:52 -0300

    Update main.dart

--- Flutter_Projetos/primeiro_projeto/lib/main.dart
+++ Flutter_Projetos/primeiro_projeto/lib/main.dart
@@ -1,61 +1,10 @@
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(const MyApp());
-}
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'PIZZARIA  DEGUSTAR',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'OLÁ BOA NOITE'),
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Quantos porcentos eduardo é gay?',
-            ),
-            Text(
-              '$_counter %',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  runApp(MaterialApp(
+      title: "Primeiro Projeto",
+      home: Container(
+        color: Colors.white,
+        child: Text("Exemplo container"),
+      )));
 }
