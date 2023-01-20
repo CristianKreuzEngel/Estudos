57ba37f29ab69f9fc55c7e30cc317ac6dfb41ed0
Cristian Kreuz Engel <kreuzengelc@gmail.com>
2023-01-20 20:57:48 -0300

    treino

--- JAVASCRIPT/test.js
+++ JAVASCRIPT/test.js
@@ -1,9 +1,29 @@
 let input =require('fs').readFileSync('./dev/stdin/flie.txt', 'utf8')
 let lines = input.split('\n')
-let t = lines.shift()
-let k = t.split(',')
-for(let i = 0; i < 100; i++){
-    console.log((`N[${i}] = ${k[0]}.${subString(k[1], 4)}`))
-    t =t / 2
-	k = t.split(',')
+
+let valor = +lines.shift()
+if(valor == 1){
+    console.log("January")
+}else if(valor == 2){
+    console.log("February")
+}else if(valor == 3){
+    console.log("March")
+}else if(valor == 4){
+    console.log("April")
+}else if(valor == 5){
+    console.log("May")
+}else if(valor == 6){
+    console.log("June")
+}else if(valor == 7){
+    console.log("July")
+}else if (valor == 8){
+    console.log("August")
+}else if (valor == 9){
+    console.log("September")
+}else if (valor == 10){
+    console.log("October")
+}else if(valor == 11){
+    console.log("November")
+}else if(valor == 12){
+    console.log("December")
 }
