fccf19c8d98cf60e374efb36f957938ebb2456ab
Cristian Kreuz Engel <108903664+CristianKreuzEngel@users.noreply.github.com>
2023-02-27 22:41:23 -0300

    estudos

--- JAVASCRIPT/test.js
+++ JAVASCRIPT/test.js
@@ -1,30 +1,15 @@
 let input =require('fs').readFileSync('./dev/stdin/flie.txt', 'utf8')
 let lines = input.split('\n')
 
-let control = 1;
-while(control != 0){
-    let [ing, people] = lines.shift().split(" ").map(a => +a)
-    if (ing == 0 && people == 0){
-        control = 0;
-        break;
-    }
-    let pessoas = lines.shift().split(" ").map(a => +a)
-    let verdadeiros = [...new Set(pessoas)];
 
-    
-    if(ing < people){
-        console.log(people - ing)
-    }else if(verdadeiros.length == 1 && pessoas.length > 1){
-        console.log(verdadeiros.length);
-    }else{
-        let soma = 0;
-        for(let i = 0; i < verdadeiros.length; i++){
-            for (let j = 0; j < people; j++){
-                if(verdadeiros[i] == pessoas[j]){
-                    soma += 1;
-                }
-            }
-        }
-        console.log(soma - verdadeiros.length)
-    }
+let name = lines.shift();
+let [dd,mm,yy] = lines.shift().split("/").map(a => +a);
+let [d,m,y] = lines.shift().split("/").map(a => +a);
+
+let calcIdade = yy - y;
+console.log(name)
+if (dd == d && mm == m){
+console.log(`Feliz aniversario!\nVoce tem ${calcIdade} anos ${name.re}`);
+}else{
+    console.log(`Voce tem ${calcIdade-1} anos ${name}${ponto}`);
 }
