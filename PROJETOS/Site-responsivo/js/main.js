c5d24e76a5c7e04ea1a1e7c41045feebf3bc3982
CristianKreuzEngel <108903664+CristianKreuzEngel@users.noreply.github.com>
2023-03-06 08:27:57 -0300

    responsa

---
+++ PROJETOS/Site-responsivo/js/main.js
@@ -0,0 +1,24 @@
+// Smooth Scrolling
+$("#navbar a, .btn").on("click", function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
+
+// Sticky menu background
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 150) {
+        document.querySelector("#navbar").style.opacity = 0.9;
+    } else {
+        document.querySelector("#navbar").style.opacity = 1;
+    }
+});
