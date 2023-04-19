262c48d5b7704d761229814cf6a259a40f694d9e
Cristian Kreuz Engel <108903664+CristianKreuzEngel@users.noreply.github.com>
2023-04-19 20:54:31 -0300

    pratica

---
+++ ESTUDO DE LINGUAGENS/C/Questões Uri/3157.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+ 
+int main() {
+	printf("Nao se refuta ciencia com opiniao\n");
+    return 0;
+}
--- ESTUDO DE LINGUAGENS/C/teste.c
+++ ESTUDO DE LINGUAGENS/C/teste.c
@@ -3,12 +3,7 @@
 #include <math.h>
 
 int main() {
-	double a,b,c,calcTrian, calcQuadrado, calcCir, calcTrapezio, calcRetan;
-	scanf("%lf %lf %lf", &a, &b, &c);
-	calcCir = (c * c) * 3.14159;
-	calcQuadrado = b * b;
-	calcRetan = a * b;
-	calcTrian = (a * c) / 2;
-	calcTrapezio = ((a+b)*c)/2;
-	printf("TRIANGULO: %.3lf\nCIRCULO: %.3lf\nTRAPEZIO: %.3lf\nQUADRADO: %.3lf\nRETANGULO: %.3lf\n", calcTrian,calcCir,calcTrapezio,calcQuadrado,calcRetan);
+	int a,b,c, maior;
+	scanf("%d %d %d", &a, &b, &c);
+	//
 }
